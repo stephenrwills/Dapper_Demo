38932aae51b3bd232b5bf6bb6e2b89bd36fca51c
Stephen Wills <stephenrwills94@gmail.com>
2022-08-02 22:47:25 -0500

    Product CRUD methods created

---
+++ Dapper_Demo/DapperProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using Dapper;
+
+namespace Dapper_Demo
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+
+
+        public void CreateProduct(string newName, double newPrice, int newCategoryID)
+        {
+            _connection.Execute("INSERT INTO Products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID;", new { name = newName, price = newPrice, categoryid = newCategoryID }); 
+        }
+
+        public void DeleteProduct(int productID)
+        {
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;",
+               new { productID = productID });
+
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;",
+               new { productID = productID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+
+        public void UpdateProductName(int newProductID, string newUpdatedName)
+        {
+            _connection.Execute("UPDATE products SET Name = @updatedName WHERE ProductID = @productID;",
+                new { updatedName = newUpdatedName, productID = newProductID });
+        }
+    }
+}
---
+++ Dapper_Demo/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Demo
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public void CreateProduct(string newName, double newPrice, int newCategoryID);
+        public void UpdateProductName(int newProductID, string newUpdatedName);
+
+        public void DeleteProduct(int productID);
+
+
+    }
+}
